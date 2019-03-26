81a1b2271e30620782e19be65048bee6bf0b7250
JingWenNg <jing_wen_ng@dell.com>
2019-03-26 20:34:46 +0800

    check in Pokemon

--- Pokemon_Type.ts
+++ Pokemon_Type.ts
@@ -18,27 +18,43 @@ function pokemonOptions(array) {
     }
 }
 
-// var myPokemon = 'Charmander';
-// var opponent = 'Pidgey';
+function pidgey(){
+    console.log('------------------------------')
 
-// you can use whatever damage formula you like, but the assignment would require:
-// 1. attacker has an attack value
-// 2. defender has a defense value
-// 3. each move (e.g. Tackle, Ember, Shadow Ball) has  a base damage value
-// 4. the final damage formula takes all 3 of the above into account
+    console.log(option[1].pokemon + ' uses ' + moves[0].name);
+    console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
+    console.log(option[1].pokemon + ' attack with value ' + option[1].attack);
+
+    myHP = myHP - moves[0].damage;
+    
+    console.log(option[0].pokemon + "'s HP is " + myHP );  
+    console.log(option[0].pokemon + " defend with value " + option[0].defend)
+  //  myHP = myHP + option[0].defend;
+
+    finalDamage = (option[1].attack-option[0].defend) *  moves[0].damage 
+    myHP = myHP - finalDamage
+    console.log('Final damage is ' + finalDamage);
+    console.log(option[0].pokemon + ' current HP is ' +myHP)
+}
 
 var myHP = 200;
 var oppHP = 100;
 var finalDamage=0;
 var isMyTurn = true;
+let ans;
+let j =0;
+var sleep=0;
 var moves = [{
         name: 'Tackle',
         damage: 5,
+        elementalType: 'Bubblebeam',
 
     },
     {
         name: 'Ember',
         damage: 8,
+        elementalType: 'Flamethrower',
+ 
     }
 ];
 
@@ -46,21 +62,19 @@ var option = [{
     pokemon: 'Charmander',
     attack: 6,
     defend: 3,
+    statusEffect: 'Poison', 
+    type: 'Fire',
 },
 {
-    pokemon: 'Pidgey',
+    pokemon: 'Squirtle',
     attack: 8,
     defend: 1,
+    statusEffect: 'Sleep',
+    type: 'Water'
 }
 ];
 
-// You can select from 2 moves, "Tackle" and "Ember"
-// "Tackle" does 5 damage
-// "Ember" does 8 damage
-// You can only pick the move for your pokemon, the wild pidgey will only use Tackle
-// Hardcode each turn
 console.log('You have encountered a pokemon battle.');
-//console.log('Your ' + option[0].pokemon + ' starts first');
 
 console.log(option[0].pokemon + ' has ' + myHP + ' HP');
 console.log(option[1].pokemon + ' has ' + oppHP + ' HP');
@@ -74,49 +88,54 @@ while (myHP > 0 && oppHP > 0)
         pokemonOptions(option);
         let ansPokemon = question('Select which pokemon turn\n')
 
-        //if select Pidgey, Pidgey use Tackle
+        //if select Squirtle , Squirtle use Tackle
         if (ansPokemon=='1')
-        {
-                console.log('------------------------------')
+        {           
+            pidgey();
 
-                console.log(option[1].pokemon + ' uses ' + moves[0].name);
-                console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
-                console.log(option[1].pokemon + ' attack with value ' + option[1].attack);
+                if(sleep==0)
+                {
+                    console.log(option[ansPokemon].pokemon+ ' initiate status effect ' + option[ansPokemon].statusEffect);
+                    console.log(option[0].pokemon + ' will sleep and not allowed to attack.');
 
-                myHP = myHP - moves[0].damage;
-                
-                console.log(option[0].pokemon + "'s HP is " + myHP );  
-                console.log(option[0].pokemon + " defend with value " + option[0].defend)
-                myHP = myHP + option[0].defend;
+                    pidgey();
 
-                finalDamage = (option[1].attack-option[0].defend) *  moves[0].damage 
-                myHP = myHP - finalDamage
-                console.log('Final damage is ' + finalDamage);
-                console.log(option[0].pokemon + ' current HP is ' +myHP)
+                    sleep=+1;
+                }
+
+                console.log(option[0].pokemon + ' is ' + option[0].type+ ' type')
+                console.log(option[ansPokemon].pokemon + ' uses '+moves[0].elementalType + ' on '+ option[0].pokemon);
+                console.log(moves[0].elementalType + ' is '+option[ansPokemon].type+' type')
+                myHP = myHP - (moves[0].damage * 2)
+
+                console.log(option[0].pokemon+' current HP is '+myHP+' after double damage. ')
 
         }else if(ansPokemon=='0'){
         displayOptions(moves);
-        let ans = question('Select type of moves\n')
+        ans = question('Select type of moves\n')
         
             if(ans=='0'){ //tackle
                     if(ansPokemon=='0'){ //Charmander
                         console.log(option[0].pokemon + ' uses ' + moves[0].name);
                         console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
-                        console.log(option[0].pokemon + ' attack with value ' + option[0].attack);
-
                         oppHP = oppHP - moves[0].damage;
                         console.log(option[1].pokemon + "'s HP is " + oppHP );
 
-                        console.log(option[1].pokemon + " defend with value " + option[1].defend)
-                        oppHP = oppHP + option[1].defend;
-        
-                        finalDamage = (option[0].attack-option[1].defend) *  moves[0].damage 
-                        oppHP = oppHP - finalDamage
-                        console.log('Final damage is ' + finalDamage);
+                        console.log(option[0].pokemon + ' attack with value ' + option[0].attack);
 
+                        console.log(option[1].pokemon + " defend with value " + option[1].defend)
+                 //       oppHP = oppHP + option[1].defend;
                         console.log(option[1].pokemon + ' current HP is ' +oppHP)
 
+                        finalDamage = (option[0].attack-option[1].defend) *  moves[0].damage      
+                        console.log('Final damage is ' + finalDamage);
+
+                        oppHP = oppHP - finalDamage
+                        console.log(option[1].pokemon + ' current HP is ' +oppHP)
+
+
                     }
+                    
             }
             else if(ans ==='1') //ember
             {
@@ -129,7 +148,7 @@ while (myHP > 0 && oppHP > 0)
                     console.log(option[1].pokemon + "'s HP is " + oppHP );
 
                     console.log(option[1].pokemon + " defend with value " + option[1].defend)
-                    oppHP = oppHP + option[1].defend;
+                 //   oppHP = oppHP + option[1].defend;
         
                     finalDamage = (option[0].attack-option[1].defend) *  moves[1].damage 
                     oppHP = oppHP - finalDamage
@@ -139,7 +158,18 @@ while (myHP > 0 && oppHP > 0)
 
                 }
             }
-               
+            //poison attack
+            if(j<5)
+            {
+                console.log(option[0].pokemon + ' initiate status effect ' +option[0].statusEffect)
+
+                            //damage = 5
+                    oppHP = oppHP - (moves[ans].damage * 0.9);
+                    console.log('Target ' + option[1].pokemon + ' pokemon HP is ' + oppHP.toFixed(2))
+                    
+            }
+            j=j+1;
+            console.log(option[0].pokemon + ' current HP is ' + myHP)
         }
 
             //break the loop if it is 0
--- Pokemon_statusEffect.ts
+++ Pokemon_statusEffect.ts
@@ -18,19 +18,32 @@ function pokemonOptions(array) {
     }
 }
 
-// var myPokemon = 'Charmander';
-// var opponent = 'Pidgey';
+function pidgey(){
+    console.log('------------------------------')
 
-// you can use whatever damage formula you like, but the assignment would require:
-// 1. attacker has an attack value
-// 2. defender has a defense value
-// 3. each move (e.g. Tackle, Ember, Shadow Ball) has  a base damage value
-// 4. the final damage formula takes all 3 of the above into account
+    console.log(option[1].pokemon + ' uses ' + moves[0].name);
+    console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
+    console.log(option[1].pokemon + ' attack with value ' + option[1].attack);
+
+    myHP = myHP - moves[0].damage;
+    
+    console.log(option[0].pokemon + "'s HP is " + myHP );  
+    console.log(option[0].pokemon + " defend with value " + option[0].defend)
+  //  myHP = myHP + option[0].defend;
+
+    finalDamage = (option[1].attack-option[0].defend) *  moves[0].damage 
+    myHP = myHP - finalDamage
+    console.log('Final damage is ' + finalDamage);
+    console.log(option[0].pokemon + ' current HP is ' +myHP)
+}
 
 var myHP = 200;
 var oppHP = 100;
 var finalDamage=0;
 var isMyTurn = true;
+let ans;
+let j =0;
+var sleep=0;
 var moves = [{
         name: 'Tackle',
         damage: 5,
@@ -46,21 +59,17 @@ var option = [{
     pokemon: 'Charmander',
     attack: 6,
     defend: 3,
+    statusEffect: 'Poison',
 },
 {
     pokemon: 'Pidgey',
     attack: 8,
     defend: 1,
+    statusEffect: 'Sleep',
 }
 ];
 
-// You can select from 2 moves, "Tackle" and "Ember"
-// "Tackle" does 5 damage
-// "Ember" does 8 damage
-// You can only pick the move for your pokemon, the wild pidgey will only use Tackle
-// Hardcode each turn
 console.log('You have encountered a pokemon battle.');
-//console.log('Your ' + option[0].pokemon + ' starts first');
 
 console.log(option[0].pokemon + ' has ' + myHP + ' HP');
 console.log(option[1].pokemon + ' has ' + oppHP + ' HP');
@@ -76,47 +85,44 @@ while (myHP > 0 && oppHP > 0)
 
         //if select Pidgey, Pidgey use Tackle
         if (ansPokemon=='1')
-        {
-                console.log('------------------------------')
+        {           
+            pidgey();
 
-                console.log(option[1].pokemon + ' uses ' + moves[0].name);
-                console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
-                console.log(option[1].pokemon + ' attack with value ' + option[1].attack);
+                if(sleep==0)
+                {
+                    console.log(option[0].pokemon + ' will sleep and not allowed to attack.')
 
-                myHP = myHP - moves[0].damage;
-                
-                console.log(option[0].pokemon + "'s HP is " + myHP );  
-                console.log(option[0].pokemon + " defend with value " + option[0].defend)
-                myHP = myHP + option[0].defend;
+                    pidgey();
 
-                finalDamage = (option[1].attack-option[0].defend) *  moves[0].damage 
-                myHP = myHP - finalDamage
-                console.log('Final damage is ' + finalDamage);
-                console.log(option[0].pokemon + ' current HP is ' +myHP)
+                    sleep=+1;
+                }
 
         }else if(ansPokemon=='0'){
         displayOptions(moves);
-        let ans = question('Select type of moves\n')
+        ans = question('Select type of moves\n')
         
             if(ans=='0'){ //tackle
                     if(ansPokemon=='0'){ //Charmander
                         console.log(option[0].pokemon + ' uses ' + moves[0].name);
                         console.log(moves[0].name + ' hits for ' + moves[0].damage + ' damage');
-                        console.log(option[0].pokemon + ' attack with value ' + option[0].attack);
-
                         oppHP = oppHP - moves[0].damage;
                         console.log(option[1].pokemon + "'s HP is " + oppHP );
 
-                        console.log(option[1].pokemon + " defend with value " + option[1].defend)
-                        oppHP = oppHP + option[1].defend;
-        
-                        finalDamage = (option[0].attack-option[1].defend) *  moves[0].damage 
-                        oppHP = oppHP - finalDamage
-                        console.log('Final damage is ' + finalDamage);
+                        console.log(option[0].pokemon + ' attack with value ' + option[0].attack);
 
+                        console.log(option[1].pokemon + " defend with value " + option[1].defend)
+                 //       oppHP = oppHP + option[1].defend;
                         console.log(option[1].pokemon + ' current HP is ' +oppHP)
 
+                        finalDamage = (option[0].attack-option[1].defend) *  moves[0].damage      
+                        console.log('Final damage is ' + finalDamage);
+
+                        oppHP = oppHP - finalDamage
+                        console.log(option[1].pokemon + ' current HP is ' +oppHP)
+
+
                     }
+                    
             }
             else if(ans ==='1') //ember
             {
@@ -129,7 +135,7 @@ while (myHP > 0 && oppHP > 0)
                     console.log(option[1].pokemon + "'s HP is " + oppHP );
 
                     console.log(option[1].pokemon + " defend with value " + option[1].defend)
-                    oppHP = oppHP + option[1].defend;
+                 //   oppHP = oppHP + option[1].defend;
         
                     finalDamage = (option[0].attack-option[1].defend) *  moves[1].damage 
                     oppHP = oppHP - finalDamage
@@ -139,7 +145,18 @@ while (myHP > 0 && oppHP > 0)
 
                 }
             }
-               
+            //poison attack
+            if(j<5)
+            {
+                console.log(option[0].pokemon + ' initiate status effect ' +option[0].statusEffect)
+
+                            //damage = 5
+                    oppHP = oppHP - (moves[ans].damage * 0.9);
+                    console.log('Target ' + option[1].pokemon + ' pokemon HP is ' + oppHP.toFixed(2))
+                    
+            }
+            j=j+1;
+            console.log(option[0].pokemon + ' current HP is ' + myHP)
         }
 
             //break the loop if it is 0
